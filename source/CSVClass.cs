e9909b0f500bddcb834e8371553c0deb55681a46
Radya A <radianath2@gmail.com>
2021-10-26 03:30:49 +0700

    added basic import export function and command line handling
    Will edit readme.md later

---
+++ source/CSVClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace Commu_Kit
+{
+    public class CSVClass
+    {
+        public CSVClass(string identifier, string characterName, string source, string target)
+        {
+            Identifier = identifier;
+            CharacterName = characterName;
+            Source = source;
+            Target = target;
+        }
+        [Name("id")]
+        public String Identifier { get; set; }
+        [Name("character")]
+        public String CharacterName{ get; set; }
+        [Name("source")]
+        public String Source{ get; set; }
+        [Name("translatedstr")]
+        public String Target{ get; set; }
+    }
+
+    public class CsvMap : ClassMap<CSVClass>
+    {
+        public CsvMap()
+        {
+            Map(m => m.Identifier).Name("id");
+            Map(m => m.CharacterName).Name("character");
+            Map(m => m.Source).Name("src");
+            Map(m => m.Target).Name("translatedstr");
+
+
+
+        }
+    }
+}
