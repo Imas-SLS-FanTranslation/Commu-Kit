763e7e5d2acead0f2464e3e04d6b3d40bd38e251
faraplay <faraplay@outlook.com>
2021-10-29 20:54:13 +0100

    move uasset stuff to new class

--- source/Program.cs
+++ source/Program.cs
@@ -6,15 +6,11 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
-using UAssetAPI;
-using UAssetAPI.PropertyTypes;
 
 namespace Commu_Kit
 {
     internal class Program
     {
-        private const UE4Version UNREAL_VERSION = UE4Version.VER_UE4_24;
-
         [Verb("export", HelpText = "Extracts commu text from .uasset files")]
         private class ExportClass
         {
@@ -47,13 +43,7 @@ namespace Commu_Kit
                 outputFile = $"{Path.GetDirectoryName(filePath)}\\{Path.GetFileNameWithoutExtension(filePath)}.csv";
                 Console.WriteLine(outputFile);
             }
-
-            UAsset openFile = new UAsset(filePath, UNREAL_VERSION);
-            var Table = (openFile.Exports[0] as DataTableExport)?.Table;
-            if (Table is null)
-            {
-                throw new InvalidDataException("Could not access data table. Please check you also have the corresponding .uexp file.");
-            }
+            var table = new UAssetTable(filePath);
             var quoteIndexes = new int[] { 3, 4 };
             var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
@@ -62,8 +52,7 @@ namespace Commu_Kit
             };
             using (var csvExporter = new CsvWriter(new StreamWriter(outputFile), CultureInfo.InvariantCulture))
             {
-                var records = Table.Data.Select((entry) => new CSVClass(entry));
-                csvExporter.WriteRecords(records);
+                csvExporter.WriteRecords(table.GetTrimmedData());
             }
             Console.WriteLine("Written to:" + outputFile);
         }
@@ -76,12 +65,7 @@ namespace Commu_Kit
             {
                 outputFile = $"{Path.GetDirectoryName(filePath)}\\{Path.GetFileNameWithoutExtension(filePath)}-NEW.uasset";
             }
-            UAsset openFile = new UAsset(opt.UassetInputPath, UNREAL_VERSION);
-            var Table = (openFile.Exports[0] as DataTableExport)?.Table;
-            if (Table is null)
-            {
-                throw new InvalidDataException("Could not access data table. Please check you also have the corresponding .uexp file.");
-            }
+            var table = new UAssetTable(opt.UassetInputPath);
             var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 Delimiter = ",",
@@ -89,16 +73,9 @@ namespace Commu_Kit
             };
             using (var csv = new CsvReader(new StreamReader(filePath, Encoding.UTF8), csvConfig))
             {
-                var csvLookup = csv.GetRecords<CSVClass>().ToDictionary((entry) => entry.Identifier);
-                foreach (var entry in Table.Data)
-                {
-                    if (entry.Value[1] is StrPropertyData strData)
-                    {
-                        strData.Value = new FString(csvLookup[entry.Name.ToString()].Target.Replace("\r\n", "\n"));
-                    }
-                }
+                table.ApplyTrimmedData(csv.GetRecords<CSVClass>());
             }
-            openFile.Write(outputFile);
+            table.WriteUAssetToFile(outputFile);
         }
 
         private static void Main(string[] args)
---
+++ source/UAssetTable.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UAssetAPI;
+using UAssetAPI.PropertyTypes;
+using UAssetAPI.StructTypes;
+
+namespace Commu_Kit
+{
+    internal class UAssetTable
+    {
+        public List<StructPropertyData> Data => table.Data;
+
+        public UAssetTable(string uAssetPath)
+        {
+            openFile = new UAsset(uAssetPath, UNREAL_VERSION);
+            table = (openFile.Exports[0] as DataTableExport)?.Table;
+            if (table is null)
+            {
+                throw new InvalidDataException("Could not access data table. Please check you also have the corresponding .uexp file.");
+            }
+        }
+
+        public IEnumerable<CSVClass> GetTrimmedData()
+        {
+            return Data.Select((entry) => new CSVClass(entry));
+        }
+
+        public void ApplyTrimmedData(IEnumerable<CSVClass> trimmedData)
+        {
+            var csvLookup = trimmedData.ToDictionary((entry) => entry.Identifier);
+            foreach (var entry in Data)
+            {
+                if (entry.Value[1] is StrPropertyData strData)
+                {
+                    strData.Value = new FString(csvLookup[entry.Name.ToString()].Target.Replace("\r\n", "\n"));
+                }
+            }
+        }
+
+        public void WriteUAssetToFile(string outputPath)
+        {
+            openFile.Write(outputPath);
+        }
+
+        private const UE4Version UNREAL_VERSION = UE4Version.VER_UE4_24;
+        private readonly UAsset openFile;
+        private readonly DataTable table;
+    }
+}
